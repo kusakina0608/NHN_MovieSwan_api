63b87e80e305ba2f0fe573ed95fe956df657b909
조준호 <junocho21@gmail.com>
2021-02-15 12:16:30 +0900

    [Feat] 세션 확인, ID 확인 API 추가, #신입-베이스캠프-8기-루키백조TF/83 #신입-베이스캠프-8기-루키백조TF/84

--- movieswan-user-api/src/main/java/com/nhn/rookie8/movieswanuserapi/controller/UserController.java
+++ movieswan-user-api/src/main/java/com/nhn/rookie8/movieswanuserapi/controller/UserController.java
@@ -2,6 +2,7 @@ package com.nhn.rookie8.movieswanuserapi.controller;
 
 
 import com.nhn.rookie8.movieswanuserapi.dto.UserDTO;
+import com.nhn.rookie8.movieswanuserapi.lib.CommonResponse;
 import com.nhn.rookie8.movieswanuserapi.service.UserService;
 import lombok.extern.log4j.Log4j2;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
+import java.util.regex.*;
 
 
 @RestController
@@ -22,7 +24,7 @@ public class UserController {
     UserService userService;
 
     @PostMapping("/register")
-    public void register(HttpServletRequest request) {
+    public CommonResponse register(HttpServletRequest request) {
 
         String uid = request.getParameter("uid");
         String password = request.getParameter("password");
@@ -43,14 +45,25 @@ public class UserController {
 
         userService.register(userDTO);
 
+        CommonResponse commonResponse = CommonResponse.builder()
+                .httpCode(200)
+                .error(false)
+                .message("Register success.")
+                .build();
+
+        return commonResponse;
     }
 
     @PostMapping("/login")
-    public String login(HttpServletRequest request){
+    public CommonResponse login(HttpServletRequest request){
 
         if(request.getSession(false) != null) {
-            System.out.println("already session exist");
-            return null;
+            CommonResponse commonResponse = CommonResponse.builder()
+                    .httpCode(400)
+                    .error(true)
+                    .message("Already session exist.")
+                    .build();
+            return commonResponse;
         }
 
         String uid = request.getParameter("uid");
@@ -59,27 +72,53 @@ public class UserController {
         UserDTO userDTO = userService.getUserInfoById(uid);
 
         if(userDTO == null){
-            return "{\"error\":\"true\",\"errorCode\":1,\"message\":\"Incorrect Id\"}";
+            CommonResponse commonResponse = CommonResponse.builder()
+                    .httpCode(400)
+                    .error(true)
+                    .message("Incorrect ID.")
+                    .content(1)
+                    .build();
+            return commonResponse;
         }
 
         if(!userDTO.getPassword().equals(password)){
-            return "{\"error\":\"true\",\"errorCode\":2,\"message\":\"Incorrect Password\"}";
+            CommonResponse commonResponse = CommonResponse.builder()
+                    .httpCode(400)
+                    .error(true)
+                    .message("Incorrect password.")
+                    .content(2)
+                    .build();
+            return commonResponse;
         }
 
         HttpSession session = request.getSession();
         session.setAttribute("uid",uid);
 
-        return "{\"error\":\"false\",\"errorCode\":,\"message\":\"Login Successfully\"}";
+        CommonResponse commonResponse = CommonResponse.builder()
+                .httpCode(200)
+                .error(false)
+                .message("Login Successfully.")
+                .build();
+
+        return commonResponse;
+
     }
 
     @GetMapping("/getUserInfo")
-    public UserDTO getUserInfo(HttpServletRequest request) {
+    public CommonResponse getUserInfo(HttpServletRequest request) {
 
         HttpSession session = request.getSession(false);
 
         if(session == null) {
             System.out.println("no session");
-            return null;
+
+            CommonResponse commonResponse = CommonResponse.builder()
+                    .httpCode(401)
+                    .error(true)
+                    .message("Login required.")
+                    .build();
+
+            return commonResponse;
         }
 
         String uid = (String) session.getAttribute("uid");
@@ -92,17 +131,30 @@ public class UserController {
                 .url(userDTO.getUrl())
                 .build();
 
-        return result;
+        CommonResponse commonResponse = CommonResponse.builder()
+                .httpCode(200)
+                .error(false)
+                .message("success")
+                .content(result)
+                .build();
+
+        return commonResponse;
     }
 
     @PutMapping("/updateUserInfo")
-    public String updateUserInfo(HttpServletRequest request) {
+    public CommonResponse updateUserInfo(HttpServletRequest request) {
 
         HttpSession session = request.getSession(false);
 
         if(session == null) {
-            System.out.println("no session");
-            return null;
+
+            CommonResponse commonResponse = CommonResponse.builder()
+                    .httpCode(401)
+                    .error(true)
+                    .message("Login required.")
+                    .build();
+
+            return commonResponse;
         }
 
         String uid = (String) session.getAttribute("uid");
@@ -125,36 +177,62 @@ public class UserController {
 
         userService.update(result);
 
-        return "success";
+        CommonResponse commonResponse = CommonResponse.builder()
+                .httpCode(200)
+                .error(false)
+                .message("Update success.")
+                .build();
+
+        return commonResponse;
     }
 
     @DeleteMapping("/deleteUser")
-    public String deleteUser(HttpServletRequest request) {
+    public CommonResponse deleteUser(HttpServletRequest request) {
 
         HttpSession session = request.getSession(false);
 
         if(session == null) {
-            System.out.println("no session");
-            return null;
+
+            CommonResponse commonResponse = CommonResponse.builder()
+                    .httpCode(401)
+                    .error(true)
+                    .message("Login required.")
+                    .build();
+
+            return commonResponse;
         }
 
         String uid = request.getParameter("uid");
 
-        userService.deleteById(uid);
-
         session.invalidate();
 
-        return "success";
+        userService.deleteById(uid);
+
+
+        CommonResponse commonResponse = CommonResponse.builder()
+                .httpCode(200)
+                .error(false)
+                .message("Delete success.")
+                .build();
+
+        return commonResponse;
+
     }
 
     @PostMapping("/checkPassword")
-    public String checkPassword(HttpServletRequest request){
+    public CommonResponse checkPassword(HttpServletRequest request){
 
         HttpSession session = request.getSession(false);
 
         if(session == null) {
-            System.out.println("no session");
-            return null;
+
+            CommonResponse commonResponse = CommonResponse.builder()
+                    .httpCode(401)
+                    .error(true)
+                    .message("Login required.")
+                    .build();
+
+            return commonResponse;
         }
 
         String uid = (String) session.getAttribute("uid");
@@ -164,24 +242,51 @@ public class UserController {
         UserDTO userDTO = userService.getUserInfoById(uid);
 
         if(userDTO == null){
-            return null;
+            CommonResponse commonResponse = CommonResponse.builder()
+                    .httpCode(400)
+                    .error(true)
+                    .message("Unexpected error.")
+                    .build();
+
+            return commonResponse;
         }
 
         if(!userDTO.getPassword().equals(password)){
-            return "{\"error\":\"true\",\"errorCode\":2,\"message\":\"Incorrect Password\"}";
+
+            CommonResponse commonResponse = CommonResponse.builder()
+                    .httpCode(400)
+                    .error(true)
+                    .message("Incorrect password.")
+                    .content(2)
+                    .build();
+
+            return commonResponse;
         }
 
-        return "{\"error\":\"false\",\"errorCode\":,\"message\":\"Check Success\"}";
+        CommonResponse commonResponse = CommonResponse.builder()
+                .httpCode(200)
+                .error(false)
+                .message("Check Success.")
+                .build();
+
+        return commonResponse;
+
     }
 
     @PutMapping("/updatePassword")
-    public String updatePassword(HttpServletRequest request) {
+    public CommonResponse updatePassword(HttpServletRequest request) {
 
         HttpSession session = request.getSession(false);
 
         if(session == null) {
-            System.out.println("no session");
-            return null;
+
+            CommonResponse commonResponse = CommonResponse.builder()
+                    .httpCode(401)
+                    .error(true)
+                    .message("Login required.")
+                    .build();
+
+            return commonResponse;
         }
 
         String uid = (String) session.getAttribute("uid");
@@ -203,18 +308,103 @@ public class UserController {
 
         userService.register(result);
 
-        return "success";
+        CommonResponse commonResponse = CommonResponse.builder()
+                .httpCode(200)
+                .error(false)
+                .message("Update password success.")
+                .build();
+
+        return commonResponse;
     }
 
     @GetMapping("/logout")
-    public void logout(HttpServletRequest request){
+    public CommonResponse logout(HttpServletRequest request){
 
         HttpSession session = request.getSession();
 
-        if(session == null) return;
+        if(session == null) {
+
+            CommonResponse commonResponse = CommonResponse.builder()
+                    .httpCode(401)
+                    .error(true)
+                    .message("Login required.")
+                    .build();
+
+            return commonResponse;
+        }
 
         session.invalidate();
 
+        CommonResponse commonResponse = CommonResponse.builder()
+                .httpCode(200)
+                .error(false)
+                .message("Logout success.")
+                .build();
+
+        return commonResponse;
+
     }
 
+    @PostMapping("/isLoggedIn")
+    public CommonResponse isLoggedIn(HttpServletRequest request){
+
+        HttpSession session = request.getSession();
+
+        if(session == null) {
+
+            CommonResponse commonResponse = CommonResponse.builder()
+                    .httpCode(401)
+                    .error(true)
+                    .message("Login required.")
+                    .build();
+
+            return commonResponse;
+        }
+
+        String uid = (String) session.getAttribute("uid");
+
+        UserDTO result = UserDTO.builder()
+                .uid(uid)
+                .build();
+
+        CommonResponse commonResponse = CommonResponse.builder()
+                .httpCode(200)
+                .error(false)
+                .message("Session exist")
+                .content(result)
+                .build();
+
+        return commonResponse;
+
+    }
+
+    @GetMapping("/isValidId")
+    public CommonResponse isValidId(@RequestParam String uid){
+
+        //Need to strict
+        Pattern pattern = Pattern.compile("(^[a-z0-9._-]{6,20}$)");
+
+        Matcher matcher = pattern.matcher(uid);
+        if(matcher.find()){
+            CommonResponse commonResponse = CommonResponse.builder()
+                    .httpCode(200)
+                    .error(false)
+                    .message("Valid ID.")
+                    .content(uid)
+                    .build();
+
+            return commonResponse;
+
+        }
+
+        CommonResponse commonResponse = CommonResponse.builder()
+                .httpCode(400)
+                .error(true)
+                .message("Invalid ID.")
+                .content(uid)
+                .build();
+
+        return commonResponse;
+
+    }
 }
